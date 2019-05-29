6ba05df17fa0937cd4bb26ced4d112e6f38fde25
赵成明 <zhaochengming@meituan.com>
2019-05-29 19:28:10 +0800

    rearrange model part Signed-off-by: 赵成明 <zhaochengming@meituan.com>

--- deep_speech_by_audier/model/speech.py
+++ deep_speech_by_audier/model/speech.py
@@ -6,14 +6,12 @@ from tensorflow.python.keras.models import Model
 from tensorflow.python.keras import backend as K
 
 
-class DFCNN(Model):
-    def __init__(self, n_classes: int, n_features: int):
-        super(DFCNN, self).__init__()  # tf 1.11 no need for `inputs` and `outputs`
+class DFCNN(object):
+    def __init__(self, n_classes: int):
         self.n_classes = n_classes
-        self.n_features = n_features
 
-    def call(self, inputs, mask=None):
-        h = self.cnn_cell(32, inputs, pool=True, input_shape=(None, self.n_features, 1))
+    def forward(self, inputs):
+        h = self.cnn_cell(32, inputs, pool=True)
         h = self.cnn_cell(64, h, pool=True)
         h = self.cnn_cell(128, h, pool=True)
         h = self.cnn_cell(128, h, pool=False)
@@ -25,9 +23,9 @@ class DFCNN(Model):
         return self.dense(self.n_classes, activation="softmax")(h)
 
     @staticmethod
-    def conv2d(size, **kwargs):
+    def conv2d(size):
         return Conv2D(filters=size, kernel_size=(3, 3), use_bias=True, activation="relu", padding="same",
-                      kernel_initializer="he_normal", **kwargs)
+                      kernel_initializer="he_normal")
     @staticmethod
     def norm(x):
         return BatchNormalization(axis=-1)(x)
@@ -37,29 +35,27 @@ class DFCNN(Model):
         return MaxPooling2D(pool_size=(2, 2), strides=None, padding="valid")(x)
 
     @staticmethod
-    def dense(units, activation="relu", **kwargs):
+    def dense(units, activation="relu"):
         return Dense(units=units, activation=activation, use_bias=True, kernel_initializer="he_normal")
 
-    def cnn_cell(self, size, x, pool=True, **kwargs):
-        x = self.norm(self.conv2d(size, **kwargs)(x))
+    def cnn_cell(self, size, x, pool=True):
+        x = self.norm(self.conv2d(size)(x))
         x = self.norm(self.conv2d(size)(x))
         if pool:
             x = self.maxpool(x)
         return x
 
 
-class DFSMN(Model):
+class DFSMN(object):
     pass
 
 
-class BiGRU(Model):
-    def __init__(self, n_classes: int, n_features: int):
-        super(BiGRU, self).__init__()
+class BiGRU(object):
+    def __init__(self, n_classes: int):
         self.n_classes = n_classes
-        self.n_features = n_features
 
-    def call(self, inputs, mask=None):
-        h = Flatten(input_shape=(None, self.n_features, 1))(inputs)
+    def forward(self, inputs):
+        h = Flatten()(inputs)
         h = self.dense(512, h)
         h = self.dense(512, h)
         h = self.bi_gru(512, h)
@@ -104,11 +100,11 @@ class AcousticModel(object):
         self.n_features = n_features
 
         if inference_model.upper() == "DFCNN":
-            self.inference_model = DFCNN(vocab_size, n_features)
+            self.inference_model = DFCNN(vocab_size)
         # elif inference_model.upper() == "DFSMN":
         #     self.inference_model = DFSMN(vocab_size, n_features)
         else:
-            self.inference_model = BiGRU(vocab_size, n_features)
+            self.inference_model = BiGRU(vocab_size)
         self.lr = learning_rate
         self._build_model()
         if is_training:
@@ -121,7 +117,7 @@ class AcousticModel(object):
             self.input_length = Input(shape=[1], dtype="int32")
             self.label_length = Input(shape=[1], dtype="int32")
         with tf.name_scope("inference"):
-            self.y_pred = self.inference_model.call(self.inputs)
+            self.y_pred = self.inference_model.forward(self.inputs)
         with tf.name_scope("loss"):
             self.loss = self.ctc_loss(self.labels, self.y_pred, self.input_length, self.label_length)
             self.model = Model(inputs=[self.labels, self.y_pred, self.input_length, self.label_length], outputs=self.loss)
