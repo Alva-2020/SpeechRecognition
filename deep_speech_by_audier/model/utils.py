04f9a06cc5a6429e288f6b5daa805011fa7db17d
赵成明 <zhaochengming@meituan.com>
2019-05-30 12:05:57 +0800

    delete import tf since its importion will cause keras import error Signed-off-by: 赵成明 <zhaochengming@meituan.com>

--- deep_speech_by_audier/model/utils.py
+++ deep_speech_by_audier/model/utils.py
@@ -1,15 +1,14 @@
 
 import numpy as np
 import tensorflow as tf
-import tensorflow.python.keras.backend as K
 from typing import Dict
 
 
 def decode_ctc(num_result: np.ndarray, id2word: Dict[int, str]):
     result = num_result[:, :, :]
     in_len = [result.shape[1]]
-    r, _ = K.ctc_decode(result, input_length=in_len, greedy=True, beam_width=10, top_paths=1)
-    r1 = K.get_value(r[0])
+    r, _ = tf.keras.backend.ctc_decode(result, input_length=in_len, greedy=True, beam_width=10, top_paths=1)
+    r1 = tf.keras.backend.get_value(r[0])
     encoded = r1[0]
     text = [id2word[x] for x in encoded]
     return r1, text
