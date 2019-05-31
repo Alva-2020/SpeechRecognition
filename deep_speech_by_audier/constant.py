27d2946505f4805ff9de1810ec9ce62652a27b8f
赵成明 <zhaochengming@meituan.com>
2019-05-31 18:56:59 +0800

    mkdir for log dir Signed-off-by: 赵成明 <zhaochengming@meituan.com>

--- deep_speech_by_audier/constant.py
+++ deep_speech_by_audier/constant.py
@@ -11,6 +11,7 @@ DATA_SOURCE_DIR = r"F:\for learn\data_source\Speech" if system == "windows"\
     else "/data/zhaochengming/data/data_source/Speech"
 
 AM_LOG_DIR = os.path.join(os.path.dirname(__file__), "AM_Model_LOG")
+os.makedirs(AM_LOG_DIR, exist_ok=True)
 AM_MODEL_DIR = os.path.join(AM_LOG_DIR, "am_model.h5")
 
 
