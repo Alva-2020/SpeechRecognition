60da861997796d5ccdceaeabeb130d53c9818a21
赵成明 <zhaochengming@meituan.com>
2019-06-05 20:21:21 +0800

    fix constant Signed-off-by: 赵成明 <zhaochengming@meituan.com>

--- deep_speech_by_audier/constant.py
+++ deep_speech_by_audier/constant.py
@@ -30,7 +30,7 @@ def make_vocab() -> Dict[str, int]:
     all_pinyin = sorted(list(all_pinyin))
     constant_values = ["_"]
     all_numbers = [str(i) for i in range(10)]
-    all_letters = [chr(x) for x in range(ord("a"), ord("z") + 1)]
+    all_letters = [chr(x) for x in range(ord("A"), ord("Z") + 1)]
     all_labels = constant_values + all_numbers + all_letters + all_pinyin
     return {pny: i for i, pny in enumerate(all_labels)}  # 1585
 
