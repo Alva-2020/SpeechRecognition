c08dce31c640c6718c242f652bde0151bc61bce9
赵成明 <zhaochengming@meituan.com>
2019-08-07 17:57:02 +0800

    add new shell Signed-off-by: 赵成明 <zhaochengming@meituan.com>

---
+++ deep_speech2/shells/run_train.sh
@@ -0,0 +1,33 @@
+cd /data/zhaochengming/projects/SpeechRecognition
+
+PYTHONPATH=.:$PYTHONPATH
+
+python main.py \
+--param_file='/data/zhaochengming/projects/SpeechRecognition/deep_speech2/settings.xml' \
+--data_file='/data/zhaochengming/data/data_source/Speech/labeled_data.txt' \
+--vocab_file='/data/zhaochengming/data/data_source/Speech/vocab.txt' \
+--vocab_type='pny' \
+--mean_std_file='/data/zhaochengming/data/data_source/Speech/mean_std.npz' \
+--sample_rate=16000 \
+--window_ms=20 \
+--stride_ms=10 \
+--specgram_type='linear' \
+--use_dB_normalization=True \
+--rnn_hidden_size=800 \
+--rnn_hideen_layers=5 \
+--rnn_type='gru' \
+--fc_use_bias=False \
+--model_dir='/data/zhaochengming/projects/SpeechRecognition/deep_speech2/AM_Model_LOG' \
+--random_seed=0 \
+--epochs=100 \
+--batch_size=4 \
+--learning_rate=0.0005
+
+
+if [ $? -ne 0 ]; then
+    echo "Failed in training!"
+    exit 1
+fi
+
+
+exit 0
