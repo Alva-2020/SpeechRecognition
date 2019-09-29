4bc653d5379dd51630639f238d6300dd359b1921
赵成明 <zhaochengming@meituan.com>
2019-09-29 18:45:35 +0800

    fix bugs in parse path Signed-off-by: 赵成明 <zhaochengming@meituan.com>

--- asrt_keras/data/data_prepare.py
+++ asrt_keras/data/data_prepare.py
@@ -8,8 +8,8 @@ from utils.data import THCHS30Loader, AiShellLoader, stream_write
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--thchs30", type=str, help="where to load thchs source and correction file", nargs="*")
-    parser.add_argument("--aishell", type=str, help="where to load aishell source and correction file", nargs="*")
+    parser.add_argument("--thchs30", type=str, help="where to load thchs source and correction file")
+    parser.add_argument("--aishell", type=str, help="where to load aishell source and correction file")
     parser.add_argument("--output", type=str, help="where to save result")
     args = parser.parse_args()
 
