21b9c090bd59ac959ce5933c9ddd1436722851d3
赵成明 <zhaochengming@meituan.com>
2019-09-29 18:37:31 +0800

    add data generate sh Signed-off-by: 赵成明 <zhaochengming@meituan.com>

---
+++ asrt_keras/run_data.sh
@@ -0,0 +1,24 @@
+
+cd /data/zhaochengming/projects/SpeechRecognition
+PYTHONPATH=.:$PYTHONPATH
+
+python asrt_keras/data/vocab_prepare.py \
+--output='/data/zhaochengming/projects/SpeechRecognition/asrt_keras/vocab.txt'
+
+if [ $? -ne 0 ]; then
+    echo "Failed in generating vocab!"
+    exit 1
+fi
+
+
+python asrt_keras/data/data_prepare.py \
+--thchs30='/data/zhaochengming/data/data_source/Speech/THCHS30/data_thchs30' \
+--aishell='/data/zhaochengming/data/data_source/Speech/aishell/data_aishell' \
+--output='/data/zhaochengming/data/data_source/Speech/labeled_data.txt'
+
+if [ $? -ne 0 ]; then
+    echo "Failed in generating files!"
+    exit 1
+fi
+
+exit 0
