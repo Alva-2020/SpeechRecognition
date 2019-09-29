47692c987f27f645eb5412297777978be3550c1d
赵成明 <zhaochengming@meituan.com>
2019-09-29 18:47:31 +0800

    add rm lines in generate vocab Signed-off-by: 赵成明 <zhaochengming@meituan.com>

--- asrt_keras/data/vocab_prepare.py
+++ asrt_keras/data/vocab_prepare.py
@@ -8,6 +8,7 @@ from _utils.nlp import ALL_PNYS, LETTERS, NUMBERS
 
 def generate_vocab_file(file: str):
     os.makedirs(os.path.dirname(file), exist_ok=True)
+    os.remove(file)
     vocab = [" "] + ALL_PNYS + LETTERS + NUMBERS + ["_"]
     assert len(vocab) == len(ALL_PNYS) + len(LETTERS) + len(NUMBERS) + 2, "Invalid vocab, there are duplicates."
     with open(file, "w", encoding="utf-8") as f:
