bf79243b3a2f2c545881f624a0fbf6d02dba1985
赵成明 <zhaochengming@meituan.com>
2019-09-30 09:43:48 +0800

    add train.sh Signed-off-by: 赵成明 <zhaochengming@meituan.com>

---
+++ asrt_keras/run_train.sh
@@ -0,0 +1,24 @@
+
+cd /data/zhaochengming/projects/SpeechRecognition
+PYTHONPATH=.:$PYTHONPATH
+
+python train.py \
+--data_file='/data/zhaochengming/data/data_source/Speech/labeled_data.txt' \
+--vocab_file='/data/zhaochengming/data/data_source/Speech/vocab.txt' \
+--model_dir='/data/zhaochengming/projects/SpeechRecognition/asrt_keras/model_log' \
+--sortagrad=True \
+--sample_rate=16000 \
+--window_ms=20 \
+--stride_ms=10 \
+--is_normalize=True \
+--seed=1 \
+--batch_size=32 \
+--learning_rate=0.001 \
+--epochs=100 \
+--gpu_num=1
+
+
+if [ $? -ne 0 ]; then
+    echo "Failed in training!"
+    exit 1
+fi
